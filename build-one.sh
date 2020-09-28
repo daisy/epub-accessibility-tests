dd15f1f514341ff6442a249d2905ba0d13e60ea5
Marisa DeMeglio <marisa.demeglio@gmail.com>
2020-09-27 23:29:48 -0700

    Adjust build process

--- build-one.sh
+++ build-one.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-EPUBCHECK="/Users/marisa/Downloads/epubcheck-4.2.0-rc/epubcheck.jar"
+source .env
 if [ -z "$1" ]
   then
     echo "Error: No epub directory supplied"
@@ -8,7 +8,7 @@ fi
 
 epubdir=$1
 
-java -jar ${EPUBCHECK} --mode exp --save ${epubdir}
+java -jar $EPUBCHECK --mode exp --save ${epubdir}
 wait
 version=`cat ${epubdir}/EPUB/package.opf | xpath '//meta[@property="schema:version"]/text()' 2>/dev/null`
 title=`cat ${epubdir}/EPUB/package.opf | xpath '//dc:title/text()' 2>/dev/null`
